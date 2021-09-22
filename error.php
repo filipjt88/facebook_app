47913de838226124fe8b2030b9a385bb78c54ffe
Filip Jotic <33574046+filipjt88@users.noreply.github.com>
2021-09-23 00:22:35 +0200

    Update error.php

--- error.php
+++ error.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Error</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-    <h1>Doslo je do greske</h1>
+    <h1 class='text-center text-danger'>An error has occurred</h1>
         <div class="row">
             <div class="col-md-12">
                 <!-- <img src="images/" alt=""> -->
