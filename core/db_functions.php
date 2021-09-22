ed9e8ac9976ff342c11f03d6e7a44212d88d32fc
Filip Jotic <33574046+filipjt88@users.noreply.github.com>
2021-09-23 00:11:22 +0200

    Update db_functions.php

--- core/db_functions.php
+++ core/db_functions.php
@@ -6,7 +6,7 @@ function register_user($title,$first_name,$last_name,$email,$password) {
     $password_hash = password_hash($password,PASSWORD_DEFAULT);
     $sql = $db->prepare("INSERT INTO users(title,first_name,last_name,email,password,role) VALUES (?,?,?,?,?,?)"); 
     $sql->bind_param('ssssss',$title,$first_name,$last_name,$email,$password_hash,$role); 
-    $sql->execute(); // izvrsi
+    $sql->execute();
 
     if($sql->errno == 0){ //errno je error number
         $_SESSION["id"] = $sql->insert_id;
